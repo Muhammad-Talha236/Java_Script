9383f638cd36a495e39ddf035f301d8c4d3aedc4
Muhammad-Talha236 <mtalha.mt236@gmail.com>
2026-07-08 09:02:05 -0700

    just created files

---
+++ first.js
@@ -0,0 +1,16 @@
+let number= 10
+console.log(number)
+
+number = "string"
+console.log(number)
+
+var hello = 10
+console.log(hello)
+
+hello = "srging"
+console.log(hello)
+
+var hello = 2.3
+console.log(hello)
+let number = "hello"
+console.log(number)
